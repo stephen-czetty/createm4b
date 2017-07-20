8785281e85b9cbc1bada950b7053bad73071c2ec
Czetty, Stephen <czettys@advisory.com>
2017-07-20 13:29:11 -0500

    Mp3 validation: Recognize and skip over ID3 tags.
    This prevents validation failures in some cases where there's
    a MP3 header in the ID3 data for some reason.

--- createm4b/audiosourcefactory.py
+++ createm4b/audiosourcefactory.py
@@ -4,6 +4,7 @@ from .mp3 import Mp3
 
 
 def get_audio_source(file_name):
+    # TODO: Mp3.is_valid gets called twice, once here, once in Mp3.__init__.
     if Mp3.is_valid(file_name):
         return Mp3(file_name)
     raise Exception("Error loading file {0}".format(file_name))
--- createm4b/mp3.py
+++ createm4b/mp3.py
@@ -63,11 +63,18 @@ class Mp3(AudioSource):
 
     @staticmethod
     def is_valid(file_path):
-        is_valid = True
-
+        f = None
         try:
             f = open(file_path, 'rb')
             block = f.read(1024)
+            # Check for an ID3 header
+            if block[0:3].decode("ascii") == "ID3":
+                id3_size = block[6] << 21 | block[7] << 14 | block[8] << 7 | block[9]
+                if id3_size < 1024:
+                    block = block[id3_size + 10:]
+                else:
+                    block = f.read(1024 * (int(id3_size / 1024) + 1))[id3_size - 1014:]
+
             frame_start = block.find(255)
             block_count = 0  # abort after 64k
             while len(block) > 0 and frame_start == -1 and block_count < 64:
@@ -83,21 +90,29 @@ class Mp3(AudioSource):
                 if frame_hdr[1] & 0xe0 != 0xe0:  # validate the rest of the frame_sync bits exist
                     return False
 
-                if frame_hdr[1] & 0x18 == 0:
-                    mpeg_version = '2.5'
-                elif frame_hdr[1] & 0x18 == 0x10:
-                    mpeg_version = '2'
-                elif frame_hdr[1] & 0x18 == 0x18:
-                    mpeg_version = '1'
+                version_id = (frame_hdr[1] & 0x18) >> 3
+                if version_id == 0:
+                    # mpeg_version = '2.5'
+                    version_index = 2
+                elif version_id == 2:
+                    # mpeg_version = '2'
+                    version_index = 1
+                elif version_id == 3:
+                    # mpeg_version = '1'
+                    version_index = 0
                 else:
                     return False
 
-                if frame_hdr[1] & 6 == 2:
-                    layer_desc = 'Layer III'
-                elif frame_hdr[1] & 6 == 4:
-                    layer_desc = 'Layer II'
-                elif frame_hdr[1] & 6 == 6:
-                    layer_desc = 'Layer I'
+                layer_description = (frame_hdr[1] & 6) >> 1
+                if layer_description == 1:
+                    # layer_desc = 'Layer III'
+                    layer_index = 2
+                elif layer_description == 2:
+                    # layer_desc = 'Layer II'
+                    layer_index = 1
+                elif layer_description == 3:
+                    # layer_desc = 'Layer I'
+                    layer_index = 0
                 else:
                     return False
 
@@ -105,18 +120,22 @@ class Mp3(AudioSource):
                 if bitrate_index == 15:
                     return False
 
-                if mpeg_version == '1':
-                    if layer_desc == 'Layer I':
-                        bitrate_col = 0
-                    elif layer_desc == 'Layer II':
-                        bitrate_col = 1
-                    else:
-                        bitrate_col = 2
-                else:
-                    if layer_desc == 'Layer I':
-                        bitrate_col = 3
-                    else:
-                        bitrate_col = 4
+                # if mpeg_version == '1':
+                #     if layer_desc == 'Layer I':
+                #         bitrate_col = 0
+                #     elif layer_desc == 'Layer II':
+                #         bitrate_col = 1
+                #     else:
+                #         bitrate_col = 2
+                # else:
+                #     if layer_desc == 'Layer I':
+                #         bitrate_col = 3
+                #     else:
+                #         bitrate_col = 4
+                bitrate_col = layer_index
+                if version_index > 0:
+                    bitrate_col += 3 + min(layer_index, 1)
+
                 bitrate = Mp3.__bitrate_chart[bitrate_index][bitrate_col]
                 if bitrate <= 0:
                     return False
@@ -125,38 +144,29 @@ class Mp3(AudioSource):
                 if sample_rate_index == 3:
                     return False
 
-                if mpeg_version == '1':
-                    sample_rate_col = 0
-                elif mpeg_version == '2':
-                    sample_rate_col = 1
-                else:
-                    sample_rate_col = 2
-                sample_rate = Mp3.__sample_rate_chart[sample_rate_index][sample_rate_col]
+                sample_rate = Mp3.__sample_rate_chart[sample_rate_index][version_index]
 
                 padding = frame_hdr[2] & 2 == 2
 
-                padding_length = 0
-                if layer_desc == 'Layer I':
-                    if padding:
-                        padding_length = 4
+                padding_length = 1 if padding else 0
+                if layer_index == 0:
+                    padding_length *= 4
                     frame_length = int((12 * bitrate * 1000 / sample_rate + padding_length) * 4)
                 else:
-                    if padding:
-                        padding_length = 1
                     frame_length = int(144 * bitrate * 1000 / sample_rate + padding_length)
 
                 # Verify the next frame
                 if frame_start + frame_length < len(block):
-                    is_valid = block[frame_start + frame_length] == 255
+                    if block[frame_start + frame_length] != 255:
+                        return False
                 else:
                     offset = (frame_start + frame_length) - len(block)
                     block = f.read(1024)
-                    if len(block) > offset:
-                        is_valid = block[offset] == 255
-                    else:
-                        is_valid = False
+                    if len(block) <= offset or block[offset] != 255:
+                        return False
+
         finally:
             if f is not None:
                 f.close()
 
-        return is_valid
+        return True
