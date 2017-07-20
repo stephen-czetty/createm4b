79a523d1b4c81707a7223fb390eebe6f96adcc75
Czetty, Stephen <czettys@advisory.com>
2017-07-20 11:55:11 -0500

    More cleanup on mp3 validation.

--- createm4b/mp3.py
+++ createm4b/mp3.py
@@ -39,9 +39,31 @@ class Mp3(AudioSource):
         self.__file_name = file_name
         self.__tag = Mp3AudioFile(file_name).tag
 
+    __bitrate_chart = [
+        [0, 0, 0, 0, 0],
+        [32, 32, 32, 32, 8],
+        [64, 48, 40, 48, 16],
+        [96, 56, 48, 56, 24],
+        [128, 64, 56, 64, 32],
+        [160, 80, 64, 80, 40],
+        [192, 96, 80, 96, 40],
+        [224, 112, 96, 112, 56],
+        [256, 128, 112, 128, 64],
+        [288, 160, 128, 144, 80],
+        [320, 192, 160, 160, 96],
+        [352, 224, 192, 176, 112],
+        [384, 256, 224, 192, 128],
+        [416, 320, 256, 224, 144],
+        [448, 384, 320, 256, 160]]
+
+    __sample_rate_chart = [
+        [44100, 22050, 11025],
+        [48000, 24000, 12000],
+        [32000, 16000, 8000]]
+
     @staticmethod
     def is_valid(file_path):
-        is_valid = False
+        is_valid = True
 
         try:
             f = open(file_path, 'rb')
@@ -55,112 +77,84 @@ class Mp3(AudioSource):
 
             if frame_start > -1:
                 frame_hdr = block[frame_start:frame_start + 4]
-                is_valid = frame_hdr[0] == 255
+                if frame_hdr[0] != 255:
+                    return False
 
-                mpeg_version = ''
-                layer_desc = ''
-                bitrate = 0
-                sample_rate = 0
+                if frame_hdr[1] & 0xe0 != 0xe0:  # validate the rest of the frame_sync bits exist
+                    return False
 
-                if is_valid:
-                    is_valid = frame_hdr[1] & 0xe0 == 0xe0  # validate the rest of the frame_sync bits exist
+                if frame_hdr[1] & 0x18 == 0:
+                    mpeg_version = '2.5'
+                elif frame_hdr[1] & 0x18 == 0x10:
+                    mpeg_version = '2'
+                elif frame_hdr[1] & 0x18 == 0x18:
+                    mpeg_version = '1'
+                else:
+                    return False
 
-                if is_valid:
-                    if frame_hdr[1] & 0x18 == 0:
-                        mpeg_version = '2.5'
-                    elif frame_hdr[1] & 0x18 == 0x10:
-                        mpeg_version = '2'
-                    elif frame_hdr[1] & 0x18 == 0x18:
-                        mpeg_version = '1'
-                    else:
-                        is_valid = False
+                if frame_hdr[1] & 6 == 2:
+                    layer_desc = 'Layer III'
+                elif frame_hdr[1] & 6 == 4:
+                    layer_desc = 'Layer II'
+                elif frame_hdr[1] & 6 == 6:
+                    layer_desc = 'Layer I'
+                else:
+                    return False
 
-                if is_valid:
-                    if frame_hdr[1] & 6 == 2:
-                        layer_desc = 'Layer III'
-                    elif frame_hdr[1] & 6 == 4:
-                        layer_desc = 'Layer II'
-                    elif frame_hdr[1] & 6 == 6:
-                        layer_desc = 'Layer I'
-                    else:
-                        is_valid = False
+                bitrate_index = frame_hdr[2] >> 4
+                if bitrate_index == 15:
+                    return False
 
-                if is_valid:
-                    bitrate_chart = [
-                        [0, 0, 0, 0, 0],
-                        [32, 32, 32, 32, 8],
-                        [64, 48, 40, 48, 16],
-                        [96, 56, 48, 56, 24],
-                        [128, 64, 56, 64, 32],
-                        [160, 80, 64, 80, 40],
-                        [192, 96, 80, 96, 40],
-                        [224, 112, 96, 112, 56],
-                        [256, 128, 112, 128, 64],
-                        [288, 160, 128, 144, 80],
-                        [320, 192, 160, 160, 96],
-                        [352, 224, 192, 176, 112],
-                        [384, 256, 224, 192, 128],
-                        [416, 320, 256, 224, 144],
-                        [448, 384, 320, 256, 160]]
-                    bitrate_index = frame_hdr[2] >> 4
-                    if bitrate_index == 15:
-                        is_valid = False
-                    else:
-                        if mpeg_version == '1':
-                            if layer_desc == 'Layer I':
-                                bitrate_col = 0
-                            elif layer_desc == 'Layer II':
-                                bitrate_col = 1
-                            else:
-                                bitrate_col = 2
-                        else:
-                            if layer_desc == 'Layer I':
-                                bitrate_col = 3
-                            else:
-                                bitrate_col = 4
-                        bitrate = bitrate_chart[bitrate_index][bitrate_col]
-                        is_valid = bitrate > 0
-
-                if is_valid:
-                    sample_rate_chart = [
-                        [44100, 22050, 11025],
-                        [48000, 24000, 12000],
-                        [32000, 16000, 8000]]
-                    sample_rate_index = (frame_hdr[2] & 0xc) >> 2
-                    if sample_rate_index != 3:
-                        if mpeg_version == '1':
-                            sample_rate_col = 0
-                        elif mpeg_version == '2':
-                            sample_rate_col = 1
-                        else:
-                            sample_rate_col = 2
-                        sample_rate = sample_rate_chart[sample_rate_index][sample_rate_col]
-                    else:
-                        is_valid = False
-
-                if is_valid:
-                    padding = frame_hdr[2] & 2 == 2
-
-                    padding_length = 0
+                if mpeg_version == '1':
                     if layer_desc == 'Layer I':
-                        if padding:
-                            padding_length = 4
-                        frame_length = int((12 * bitrate * 1000 / sample_rate + padding_length) * 4)
+                        bitrate_col = 0
+                    elif layer_desc == 'Layer II':
+                        bitrate_col = 1
                     else:
-                        if padding:
-                            padding_length = 1
-                        frame_length = int(144 * bitrate * 1000 / sample_rate + padding_length)
+                        bitrate_col = 2
+                else:
+                    if layer_desc == 'Layer I':
+                        bitrate_col = 3
+                    else:
+                        bitrate_col = 4
+                bitrate = Mp3.__bitrate_chart[bitrate_index][bitrate_col]
+                if bitrate <= 0:
+                    return False
 
-                    # Verify the next frame
-                    if frame_start + frame_length < len(block):
-                        is_valid = block[frame_start + frame_length] == 255
+                sample_rate_index = (frame_hdr[2] & 0xc) >> 2
+                if sample_rate_index == 3:
+                    return False
+
+                if mpeg_version == '1':
+                    sample_rate_col = 0
+                elif mpeg_version == '2':
+                    sample_rate_col = 1
+                else:
+                    sample_rate_col = 2
+                sample_rate = Mp3.__sample_rate_chart[sample_rate_index][sample_rate_col]
+
+                padding = frame_hdr[2] & 2 == 2
+
+                padding_length = 0
+                if layer_desc == 'Layer I':
+                    if padding:
+                        padding_length = 4
+                    frame_length = int((12 * bitrate * 1000 / sample_rate + padding_length) * 4)
+                else:
+                    if padding:
+                        padding_length = 1
+                    frame_length = int(144 * bitrate * 1000 / sample_rate + padding_length)
+
+                # Verify the next frame
+                if frame_start + frame_length < len(block):
+                    is_valid = block[frame_start + frame_length] == 255
+                else:
+                    offset = (frame_start + frame_length) - len(block)
+                    block = f.read(1024)
+                    if len(block) > offset:
+                        is_valid = block[offset] == 255
                     else:
-                        offset = (frame_start + frame_length) - len(block)
-                        block = f.read(1024)
-                        if len(block) > offset:
-                            is_valid = block[offset] == 255
-                        else:
-                            is_valid = False
+                        is_valid = False
         finally:
             if f is not None:
                 f.close()
