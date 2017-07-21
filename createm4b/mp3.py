1066c6ed96f8eb4a2c2eb31cb141f9bc65512f3e
Czetty, Stephen <czettys@advisory.com>
2017-07-21 16:57:03 -0500

    Mp3 refactoring.
    Eventually adding full file parsing to get the duration so we can
    do away with ffprobe entirely.

--- createm4b/mp3.py
+++ createm4b/mp3.py
@@ -2,7 +2,7 @@
 
 from eyed3.mp3 import Mp3AudioFile
 import ffprobe3
-
+import io
 from .audiosource import AudioSource
 
 
@@ -39,6 +39,39 @@ class Mp3(AudioSource):
         self.__file_name = file_name
         self.__tag = Mp3AudioFile(file_name).tag
 
+    @staticmethod
+    def is_valid(file_path):
+        f = None
+        # noinspection PyBroadException
+        try:
+            f = open(file_path, 'rb')
+            block = f.read(10)
+
+            # Check for an ID3 header
+            id3 = ID3Header(block)
+            if id3.is_valid_id3:
+                f.seek(id3.size, io.SEEK_CUR)
+            else:
+                f.seek(0, io.SEEK_SET)
+
+            block = f.read(4)
+            try:
+                frame = Mp3Frame(block)
+
+                # Verify the next frame
+                f.seek(frame.frame_length - 4, io.SEEK_CUR)
+                block = f.read(4)
+                Mp3Frame(block)
+            except Mp3Exception:
+                return False
+        finally:
+            if f is not None:
+                f.close()
+
+        return True
+
+
+class Mp3Frame:
     __bitrate_chart = [
         [0, 0, 0, 0, 0],
         [32, 32, 32, 32, 8],
@@ -61,115 +94,110 @@ class Mp3(AudioSource):
         [48000, 24000, 12000],
         [32000, 16000, 8000]]
 
-    @staticmethod
-    def is_valid(file_path):
-        f = None
-        # noinspection PyBroadException
-        try:
-            f = open(file_path, 'rb')
-            block = f.read(1024)
-            # Check for an ID3 header
-            if block[0:3].decode("ascii") == "ID3":
-                id3_size = block[6] << 21 | block[7] << 14 | block[8] << 7 | block[9]
-                if id3_size < 1024:
-                    block = block[id3_size + 10:]
-                else:
-                    block = f.read(1024 * (int(id3_size / 1024) + 1))[id3_size - 1014:]
+    @property
+    def frame_length(self):
+        return self.__frame_length
 
-            frame_start = block.find(255)
-            block_count = 0  # abort after 64k
-            while len(block) > 0 and frame_start == -1 and block_count < 64:
-                block = f.read(1024)
-                frame_start = block.find(255)
-                block_count += 1
+    @property
+    def sample_rate(self):
+        return self.__sample_rate
 
-            if frame_start > -1:
-                frame_hdr = block[frame_start:frame_start + 4]
-                if frame_hdr[0] != 255:
-                    return False
+    @property
+    def bitrate(self):
+        return self.__bitrate
 
-                if frame_hdr[1] & 0xe0 != 0xe0:  # validate the rest of the frame_sync bits exist
-                    return False
+    def __init__(self, data):
+        frame_hdr = data[0:4]
+        if frame_hdr[0] != 255:
+            raise Mp3Exception("Invalid Frame Sync")
 
-                version_id = (frame_hdr[1] & 0x18) >> 3
-                if version_id == 0:
-                    # mpeg_version = '2.5'
-                    version_index = 2
-                elif version_id == 2:
-                    # mpeg_version = '2'
-                    version_index = 1
-                elif version_id == 3:
-                    # mpeg_version = '1'
-                    version_index = 0
-                else:
-                    return False
+        if frame_hdr[1] & 0xe0 != 0xe0:  # validate the rest of the frame_sync bits exist
+            raise Mp3Exception("Invalid Frame Sync")
 
-                layer_description = (frame_hdr[1] & 6) >> 1
-                if layer_description == 1:
-                    # layer_desc = 'Layer III'
-                    layer_index = 2
-                elif layer_description == 2:
-                    # layer_desc = 'Layer II'
-                    layer_index = 1
-                elif layer_description == 3:
-                    # layer_desc = 'Layer I'
-                    layer_index = 0
-                else:
-                    return False
+        version_id = (frame_hdr[1] & 0x18) >> 3
+        if version_id == 0:
+            self.__mpeg_version = '2.5'
+            version_index = 2
+        elif version_id == 2:
+            self.__mpeg_version = '2'
+            version_index = 1
+        elif version_id == 3:
+            self.__mpeg_version = '1'
+            version_index = 0
+        else:
+            raise Mp3Exception("Unknown MPEG version")
 
-                bitrate_index = frame_hdr[2] >> 4
-                if bitrate_index == 15:
-                    return False
+        layer_description = (frame_hdr[1] & 6) >> 1
+        if layer_description == 1:
+            self.__layer_desc = 'Layer III'
+            layer_index = 2
+        elif layer_description == 2:
+            self.__layer_desc = 'Layer II'
+            layer_index = 1
+        elif layer_description == 3:
+            self.__layer_desc = 'Layer I'
+            layer_index = 0
+        else:
+            raise Mp3Exception("Unknown MPEG Layer")
 
-                # if mpeg_version == '1':
-                #     if layer_desc == 'Layer I':
-                #         bitrate_col = 0
-                #     elif layer_desc == 'Layer II':
-                #         bitrate_col = 1
-                #     else:
-                #         bitrate_col = 2
-                # else:
-                #     if layer_desc == 'Layer I':
-                #         bitrate_col = 3
-                #     else:
-                #         bitrate_col = 4
-                bitrate_col = layer_index
-                if version_index > 0:
-                    bitrate_col += 3 + min(layer_index, 1)
+        bitrate_index = frame_hdr[2] >> 4
+        if bitrate_index == 15:
+            raise Mp3Exception("Unknown bitrate")
 
-                bitrate = Mp3.__bitrate_chart[bitrate_index][bitrate_col]
-                if bitrate <= 0:
-                    return False
+        # if mpeg_version == '1':
+        #     if layer_desc == 'Layer I':
+        #         bitrate_col = 0
+        #     elif layer_desc == 'Layer II':
+        #         bitrate_col = 1
+        #     else:
+        #         bitrate_col = 2
+        # else:
+        #     if layer_desc == 'Layer I':
+        #         bitrate_col = 3
+        #     else:
+        #         bitrate_col = 4
+        bitrate_col = layer_index
+        if version_index > 0:
+            bitrate_col += 3 + min(layer_index, 1)
 
-                sample_rate_index = (frame_hdr[2] & 0xc) >> 2
-                if sample_rate_index == 3:
-                    return False
+        self.__bitrate = Mp3Frame.__bitrate_chart[bitrate_index][bitrate_col]
+        if self.__bitrate <= 0:
+            raise Mp3Exception("Invalid bitrate")
 
-                sample_rate = Mp3.__sample_rate_chart[sample_rate_index][version_index]
+        sample_rate_index = (frame_hdr[2] & 0xc) >> 2
+        if sample_rate_index == 3:
+            raise Mp3Exception("Invalid sample rate")
 
-                padding = frame_hdr[2] & 2 == 2
+        self.__sample_rate = Mp3Frame.__sample_rate_chart[sample_rate_index][version_index]
 
-                padding_length = 1 if padding else 0
-                if layer_index == 0:
-                    padding_length *= 4
-                    frame_length = int((12 * bitrate * 1000 / sample_rate + padding_length) * 4)
-                else:
-                    frame_length = int(144 * bitrate * 1000 / sample_rate + padding_length)
+        padding = frame_hdr[2] & 2 == 2
 
-                # Verify the next frame
-                if frame_start + frame_length < len(block):
-                    if block[frame_start + frame_length] != 255:
-                        return False
-                else:
-                    offset = (frame_start + frame_length) - len(block)
-                    block = f.read(1024)
-                    if len(block) <= offset or block[offset] != 255:
-                        return False
-        except:
-            return False
+        padding_length = 1 if padding else 0
+        if layer_index == 0:
+            padding_length *= 4
+            self.__frame_length = int((12 * self.__bitrate * 1000 / self.__sample_rate + padding_length) * 4)
+        else:
+            self.__frame_length = int(144 * self.__bitrate * 1000 / self.__sample_rate + padding_length)
 
-        finally:
-            if f is not None:
-                f.close()
 
-        return True
+class ID3Header:
+    __id3_size = 0
+
+    @property
+    def is_valid_id3(self):
+        return self.__is_id3
+
+    @property
+    def size(self):
+        return self.__id3_size
+
+    def __init__(self, data):
+        if data[0:3].decode("ascii") != "ID3":
+            self.__is_id3 = False
+            return
+        self.__is_id3 = True
+        self.__id3_size = data[6] << 21 | data[7] << 14 | data[8] << 7 | data[9]
+
+
+class Mp3Exception(Exception):
+    pass
